e7ef9d924e79dfbb0b57a27a7d20c3fc7faebf76
Heechul Kim <jijisa@iorchard.co.kr>
2026-07-21 16:01:18 +0900

    add VER and SRC_VER argument in run.sh

--- build_artifacts/run.sh
+++ build_artifacts/run.sh
@@ -6,5 +6,5 @@ SRC_VER=${2:-0.1.0}
 podman build -t docker.io/skbcloudx/bcocinero-builder \
   --build-arg=FROM=docker.io/skbcloudx/rockylinux:${VER} .
 podman run -it --privileged -v $(pwd)/output:/output \
-  --rm docker.io/skbcloudx/bcocinero-builder
+  --rm docker.io/skbcloudx/bcocinero-builder ${VER} ${SRC_VER}
 
